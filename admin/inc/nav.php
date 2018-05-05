7d15759d796830cb5d2ec2ab6f276a8d47990090
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2018-05-05 18:19:23 +0530

    added admin pages

--- admin/inc/nav.php
+++ admin/inc/nav.php
@@ -1,29 +1,48 @@
-<div class="menu-wrap">
+			<div class="menu-wrap">
 				<div id="mobnav-btn">Menu <i class="fa fa-bars"></i></div>
 				<ul class="sf-menu">
 					<li>
-						<a href="#">Home</a>
+						<a href="index.php">Dashboard</a>
 					</li>
 					<li>
-						<a href="#">Shop</a>
+						<a href="#">Categories</a>
 						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
 						<ul>
-							<li><a href="#">Category1</a></li>
-							<li><a href="#">Category1</a></li>
-							<li><a href="#">Category1</a></li>
-							<li><a href="#">Category1</a></li>
+							<li><a href="categories.php">View Categories</a></li>
+							<li><a href="addcategory.php">Add Category</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Products</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="products.php">View Products</a></li>
+							<li><a href="addproduct.php">Add Product</a></li>
+						</ul>
+					</li>
+
+					<li>
+						<a href="#">Orders</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="orders.php">View Orders</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Customers</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="customers.php">View Customers</a></li>
+							<li><a href="reviews.php">View Reviews</a></li>
 						</ul>
 					</li>
 					<li>
 						<a href="#">My Account</a>
 						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
 						<ul>
-							<li><a href="#">My Orders</a></li>
-							<li><a href="#">Logout</a></li>
+							<li><a href="">Edit Profile</a></li>
+							<li><a href="logout.php">Logout</a></li>
 						</ul>
-					</li>
-					<li>
-						<a href="#">Contact</a>
 					</li>
 				</ul>
 				<div class="header-xtra">
