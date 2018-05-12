5d036693fe45f9898dfd8a7e25bd5279ea4b0840
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2018-05-13 03:17:03 +0530

    added category operations

--- admin/editcategory.php
+++ admin/editcategory.php
@@ -5,14 +5,21 @@
 		header('location: login.php');
 	}
 
+	if(isset($_GET) & !empty($_GET)){
+		$id = $_GET['id'];
+	}else{
+		header('location: categories.php');
+	}
+
 	if(isset($_POST) & !empty($_POST)){
+		$id = mysqli_real_escape_string($connection, $_POST['id']);
 		$name = mysqli_real_escape_string($connection, $_POST['categoryname']);
-		$sql = "INSERT INTO category (name) VALUES ('$name')";
+		$sql = "UPDATE category SET name = '$name' WHERE id=$id";
 		$res = mysqli_query($connection, $sql);
 		if($res){
-			$smsg = "Category Added";
+			$smsg = "Category Updated";
 		}else{
-			$fmsg = "Failed Add Category";
+			$fmsg = "Failed Update Category";
 		}
 	}
 ?>
@@ -27,7 +34,13 @@
 			<form method="post">
 			  <div class="form-group">
 			    <label for="Productname">Category Name</label>
-			    <input type="text" class="form-control" name="categoryname" id="Categoryname" placeholder="Category Name">
+			    <?php 	
+					$sql = "SELECT * FROM category WHERE id=$id";
+					$res = mysqli_query($connection, $sql); 
+					$r = mysqli_fetch_assoc($res); 
+				?>
+				<input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+			    <input type="text" class="form-control" name="categoryname" id="Categoryname" placeholder="Category Name" value="<?php echo $r['name'];  ?>">
 			  </div>
 			  
 			  <button type="submit" class="btn btn-default">Submit</button>
